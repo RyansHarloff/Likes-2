7dd63b5af85cb33dc95f43df66e8cbc8e5a27f6b
Ryan Harloff <ryans.harloff@gmail.com>
2021-04-27 16:54:24 -0500

    Likes-2

---
+++ script.js
@@ -0,0 +1,23 @@
+var like = 9
+var likeElement = document.querySelector('#like');
+
+function add1() {
+    like++;
+    likeElement.innerText = like + " Like(s)";
+}
+
+var like2 = 12
+var likeElement2 = document.querySelector('#like2');
+
+function add2() {
+    like2++;
+    likeElement2.innerText = like2 + " Like(s)";
+}
+
+var like3 = 9
+var likeElement3 = document.querySelector('#like3');
+
+function add3() {
+    like3++;
+    likeElement3.innerText = like3 + " Like(s)";
+}
